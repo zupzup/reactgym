4b70e6a6238d5ba49c494e9b4c880a2a63b0c79f
Mario Zupan <mzupanmz@gmail.com>
2015-04-04 00:12:14 +0200

    add tests for backuputil error

--- __tests__/utils/BackupUtil.spec.js
+++ __tests__/utils/BackupUtil.spec.js
@@ -11,10 +11,19 @@ window.requestFileSystem = (filesys, num, cb) => {
     };
     cb(fs);
 };
+window.alert = jest.genMockFunction();
+
 describe("BackupUtil", () => {
     describe("getDirectory", () => {
         it("gets a directory and triggers the callback", () => {
             expect(true).toBe(true);
+        });
+    });
+
+    describe("err", () => {
+        it("alerts [FAIL] with an error message", () => {
+            BackupUtil.err('hello')();
+            expect(window.alert.mock.calls.length).toBe(1);
         });
     });
 });
