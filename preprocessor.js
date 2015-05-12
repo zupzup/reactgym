55d2878849dc9eaf3116a39a26bbe3c0bb74bfae
Mario Zupan <mzupanmz@gmail.com>
2015-05-12 12:18:00 +0200

    disable coverage due to a bug together with babel and start using es6 modules

--- preprocessor.js
+++ preprocessor.js
@@ -1,11 +1,21 @@
 var ReactTools = require('react-tools');
-// var babel = require('babel');
+var babel = require('babel-core');
 module.exports = {
     process: function(src, file) {
         if (!file.match(/.*\/(__tests__|scripts|react-router|StubRouterContext).*/)) {
             return src;
         }
-        return ReactTools.transform(src, {harmony: true});
-        // return babel.transform("code", { experimental: true }).code;
+        var stage = process.env.BABEL_JEST_STAGE || 2;
+        var canCompile = babel.canCompile(file);
+
+        if (!canCompile) {
+            return "";
+        }
+
+        if (file.indexOf("node_modules") === -1) {
+            return babel.transform(src, {filename: file, stage: stage}).code;
+        }
+
+        return src;
     }
 };
